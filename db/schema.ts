4d1eb8b7e601fb02122a7ae9141da27701adcc6a
Andres Aguirre <aguirregzz97@gmail.com>
2025-07-18 20:45:20 -0600

    add initial db schema

--- db/schema.ts
+++ db/schema.ts
@@ -5,15 +5,19 @@ import {
   text,
   primaryKey,
   integer,
+  jsonb,
 } from "drizzle-orm/pg-core";
 import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { AdapterAccountType } from "@auth/core/adapters";
+import { relations } from "drizzle-orm";
 
 const connectionString = "postgres://postgres:postgres@localhost:5432/drizzle";
 const pool = postgres(connectionString, { max: 1 });
 
 export const db = drizzle(pool);
+
+// auth tables
 
 export const users = pgTable("user", {
   id: text("id")
@@ -96,4 +100,144 @@ export const authenticators = pgTable(
       }),
     },
   ],
+);
+
+// app tables
+
+export const quinielas = pgTable("quiniela", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  ownerId: text("ownerId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  name: text("name").notNull(),
+  description: text("description"),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const quiniela_settings = pgTable("quiniela_settings", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  quinielaId: text("quinielaId")
+    .notNull()
+    .unique()
+    .references(() => quinielas.id, { onDelete: "cascade" }),
+  prizeDistribution: jsonb("prizeDistribution")
+    .$type<{ position: number; percentage: number }[]>()
+    .notNull(),
+  allowEditPredictions: boolean("allowEditPredictions").notNull().default(true),
+  pointsForExactResultPrediction: integer("pointsForExactResultPrediction")
+    .notNull()
+    .default(2),
+  pointsForCorrectResultPrediction: integer("pointsForCorrectResultPrediction")
+    .notNull()
+    .default(1),
+  pointsForCorrectResultAndScorePrediction: integer(
+    "pointsForCorrectResultAndScorePrediction",
+  )
+    .notNull()
+    .default(3),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const quiniela_participants = pgTable("quiniela_participants", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  quinielaId: text("quinielaId")
+    .notNull()
+    .references(() => quinielas.id, { onDelete: "cascade" }),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, {
+      onDelete: "cascade",
+    }),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const matches = pgTable("matches", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  externalFixtureId: text("externalId").notNull(),
+  externalLeagueId: text("externalLeagueId").notNull(),
+  homeTeam: text("homeTeam").notNull(),
+  awayTeam: text("awayTeam").notNull(),
+  league: text("league").notNull(),
+  season: text("season").notNull(),
+  round: text("round").notNull(),
+  homeTeamScore: integer("homeTeamScore"),
+  awayTeamScore: integer("awayTeamScore"),
+  matchDate: timestamp("matchDate", { mode: "date" }).notNull(),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const quiniela_matches = pgTable("quiniela_matches", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  quinielaId: text("quinielaId")
+    .notNull()
+    .references(() => quinielas.id, { onDelete: "cascade" }),
+  matchId: text("matchId")
+    .notNull()
+    .references(() => matches.id, { onDelete: "cascade" }),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+export const predictions = pgTable("predictions", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  quinielaMatchId: text("quinielaMatchId")
+    .notNull()
+    .references(() => quiniela_matches.id, { onDelete: "cascade" }),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  predictedHomeScore: integer("predictedHomeScore"),
+  predictedAwayScore: integer("predictedAwayScore"),
+  createdAt: timestamp("createdAt", { mode: "date" }).notNull().defaultNow(),
+  updatedAt: timestamp("updatedAt", { mode: "date" }).notNull().defaultNow(),
+});
+
+// relations
+
+export const userRelations = relations(users, ({ many }) => ({
+  quinielas: many(quinielas),
+  predictions: many(predictions),
+}));
+
+export const quinielaRelations = relations(quinielas, ({ many, one }) => ({
+  owner: one(users, {
+    fields: [quinielas.ownerId],
+    references: [users.id],
+  }),
+  settings: one(quiniela_settings, {
+    fields: [quinielas.id],
+    references: [quiniela_settings.quinielaId],
+  }),
+  participants: many(quiniela_participants),
+  matches: many(quiniela_matches),
+}));
+
+export const participantsRelations = relations(
+  quiniela_participants,
+  ({ one }) => ({
+    quiniela: one(quinielas, {
+      fields: [quiniela_participants.quinielaId],
+      references: [quinielas.id],
+    }),
+    user: one(users, {
+      fields: [quiniela_participants.userId],
+      references: [users.id],
+    }),
+  }),
 );
---
+++ supabase/migrations/0000_redundant_venom.sql
@@ -0,0 +1,122 @@
+CREATE TABLE "account" (
+	"userId" text NOT NULL,
+	"type" text NOT NULL,
+	"provider" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text
+);
+--> statement-breakpoint
+CREATE TABLE "authenticator" (
+	"credentialID" text NOT NULL,
+	"userId" text NOT NULL,
+	"providerAccountId" text NOT NULL,
+	"credentialPublicKey" text NOT NULL,
+	"counter" integer NOT NULL,
+	"credentialDeviceType" text NOT NULL,
+	"credentialBackedUp" boolean NOT NULL,
+	"transports" text,
+	CONSTRAINT "authenticator_credentialID_unique" UNIQUE("credentialID")
+);
+--> statement-breakpoint
+CREATE TABLE "matches" (
+	"id" text PRIMARY KEY NOT NULL,
+	"externalId" text NOT NULL,
+	"externalLeagueId" text NOT NULL,
+	"homeTeam" text NOT NULL,
+	"awayTeam" text NOT NULL,
+	"league" text NOT NULL,
+	"season" text NOT NULL,
+	"round" text NOT NULL,
+	"homeTeamScore" integer,
+	"awayTeamScore" integer,
+	"matchDate" timestamp NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "predictions" (
+	"id" text PRIMARY KEY NOT NULL,
+	"quinielaMatchId" text NOT NULL,
+	"userId" text NOT NULL,
+	"predictedHomeScore" integer,
+	"predictedAwayScore" integer,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "quiniela_matches" (
+	"id" text PRIMARY KEY NOT NULL,
+	"quinielaId" text NOT NULL,
+	"matchId" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "quiniela_participants" (
+	"id" text PRIMARY KEY NOT NULL,
+	"quinielaId" text NOT NULL,
+	"userId" text NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "quiniela_settings" (
+	"id" text PRIMARY KEY NOT NULL,
+	"quinielaId" text NOT NULL,
+	"prizeDistribution" jsonb NOT NULL,
+	"allowEditPredictions" boolean DEFAULT true NOT NULL,
+	"pointsForExactResultPrediction" integer DEFAULT 2 NOT NULL,
+	"pointsForCorrectResultPrediction" integer DEFAULT 1 NOT NULL,
+	"pointsForCorrectResultAndScorePrediction" integer DEFAULT 3 NOT NULL,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "quiniela_settings_quinielaId_unique" UNIQUE("quinielaId")
+);
+--> statement-breakpoint
+CREATE TABLE "quiniela" (
+	"id" text PRIMARY KEY NOT NULL,
+	"ownerId" text NOT NULL,
+	"name" text NOT NULL,
+	"description" text,
+	"createdAt" timestamp DEFAULT now() NOT NULL,
+	"updatedAt" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "session" (
+	"sessionToken" text PRIMARY KEY NOT NULL,
+	"userId" text NOT NULL,
+	"expires" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"name" text,
+	"email" text,
+	"emailVerified" timestamp,
+	"image" text,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "verificationToken" (
+	"identifier" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL
+);
+--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "authenticator" ADD CONSTRAINT "authenticator_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "predictions" ADD CONSTRAINT "predictions_quinielaMatchId_quiniela_matches_id_fk" FOREIGN KEY ("quinielaMatchId") REFERENCES "public"."quiniela_matches"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "predictions" ADD CONSTRAINT "predictions_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela_matches" ADD CONSTRAINT "quiniela_matches_quinielaId_quiniela_id_fk" FOREIGN KEY ("quinielaId") REFERENCES "public"."quiniela"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela_matches" ADD CONSTRAINT "quiniela_matches_matchId_matches_id_fk" FOREIGN KEY ("matchId") REFERENCES "public"."matches"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela_participants" ADD CONSTRAINT "quiniela_participants_quinielaId_quiniela_id_fk" FOREIGN KEY ("quinielaId") REFERENCES "public"."quiniela"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela_participants" ADD CONSTRAINT "quiniela_participants_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela_settings" ADD CONSTRAINT "quiniela_settings_quinielaId_quiniela_id_fk" FOREIGN KEY ("quinielaId") REFERENCES "public"."quiniela"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "quiniela" ADD CONSTRAINT "quiniela_ownerId_user_id_fk" FOREIGN KEY ("ownerId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
